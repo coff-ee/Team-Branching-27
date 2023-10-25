1a9970a07210b66fd302f4780d186a0950e18656
coff-ee <safaa.imran7@gmail.com>
2023-10-25 09:54:48 +0100

    Update Math.java

--- Math.java
+++ Math.java
@@ -13,35 +13,35 @@ public class Math{
         }
     }
 
-    public int Min(int num1, int num2){
-        if(num1 < num2){
-            return num1;
-        } else {
-            return num2;
-        }
-    }
+    // public int Min(int num1, int num2){
+    //     if(num1 < num2){
+    //         return num1;
+    //     } else {
+    //         return num2;
+    //     }
+    // }
 
-    public int Add(int num1, int num2){
-        return num1+num2;
-    }
+    // public int Add(int num1, int num2){
+    //     return num1+num2;
+    // }
 
-    public int Sub(int num1, int num2){
-        return num1-num2;
-    }
+    // public int Sub(int num1, int num2){
+    //     return num1-num2;
+    // }
 
-    public int Multi(int num1, int num2){
-        return num1*num2;
-    }
+    // public int Multi(int num1, int num2){
+    //     return num1*num2;
+    // }
 
-    public double Divide(int num1, int num2){
-        return num1/num2;
-    }
+    // public double Divide(int num1, int num2){
+    //     return num1/num2;
+    // }
 
-    public int Power(int num1, int num2){
-        return num1^num2;
-    }
+    // public int Power(int num1, int num2){
+    //     return num1^num2;
+    // }
 
-    public int Mod(int num1, int num2){
-        return num1%num2;
-    }
+    // public int Mod(int num1, int num2){
+    //     return num1%num2;
+    // }
 }
